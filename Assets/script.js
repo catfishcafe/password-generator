02891ecbb387a7bfd379071ae700d30dfe4d1cb7
catfishcafe <akmhobbs@gmail.com>
2023-04-09 09:26:04 -0400

    outline completed with either code or comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,62 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//All the code having to do with the Generate Password button is wrapped in an anonymous function.
+// While not applicable here, if hypothetical additional scripts were added to this page, this anonymous function prevents variables in this script from intereferring with new scripts.
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+(function () {
+  // The button to generate the password is now a variable called passwordBtn
+  const passwordBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
+  // Write password to the #password input
+  function writePassword() {
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
 
+  // Clicking on the button now calls the function writePassword
+  passwordBtn.addEventListener("click", writePassword);
+
+  //MY CODE BELOW
+
+//access a random index in that array
+//specifiy how many random indeces to access
+
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const numbers = '0123456789'.split('');
+let specialChar = '!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'.split('');
+specialChar.push("'");
+
+// program to get a random item from an array
+
+function getRandomItem(specialChar) {
+
+  // get random index value
+  const specialCharRandomIndex = Math.floor(Math.random() * specialChar.length);
+
+  // get random item
+  const specialCharItems = specialChar[specialCharRandomIndex];
+
+//somehow multiply specialChar by the passwordLength variable
+
+  return item;
+ 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  let passwordLength = window.prompt(
+    'Enter desired password length (number of characters):'
+    );
+  console.log(passwordLength * 3);
+
+  if (confirm("Include special characters? (OK = yes, cancel = no)") == true) {
+    // Function to fetch the random number of indices indicated
+    text = "You pressed OK!";
+  } else {
+    text = "You canceled!";
+  };
+  return 'hey';
+}
+
+})();
